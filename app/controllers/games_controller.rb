55f3d64fa45b735d89ca066dc994d15303315ca8
c1505 <coreym247@gmail.com>
2017-09-05 14:15:26 -0400

    added games result limit

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,7 +7,15 @@ class GamesController < ApplicationController
     complexity = params[:complexity]
     players = params[:players]
     minutes = params[:time]
-    @games = Game.enough_players(players).enough_spaces(players).enough_time(minutes).complexity(complexity)
+    quantity = params[:quantity]
+    @games = Game.enough_players(players)
+            .enough_spaces(players)
+            .enough_time(minutes)
+            .complexity(complexity)
+    unless quantity.blank?
+      quantity = quantity.to_i
+      @games = @games.sample(quantity)
+    end
     render "index"
   end
 
