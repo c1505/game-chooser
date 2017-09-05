ce811bb07b5d6c1fa57bc4aacf965da657a1b362
c1505 <coreym247@gmail.com>
2017-09-05 12:09:35 -0400

    filtering by complexity, players, and time

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,13 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
+  def filter
+    complexity = params[:complexity]
+    players = params[:players]
+    minutes = params[:time]
+    @games = Game.enough_players(players).enough_spaces(players).enough_time(minutes).complexity(complexity)
+    render "index"
+  end
+
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,41 @@
 class Game < ApplicationRecord
+  def self.enough_players(players)
+    unless players.blank?
+      players = players.to_i
+      Game.where(min_players: 0..players)
+    else
+      Game.all
+    end
+  end
+
+  def self.enough_spaces(players)
+    unless players.blank?
+      players = players.to_i
+      Game.where(max_players: players..Float::INFINITY)
+    else
+      Game.all
+    end
+  end
+
+  def self.enough_time(minutes)
+    unless minutes.blank?
+      minutes = minutes.to_i
+      min_time = minutes * 0.7
+      Game.where(time: min_time..minutes)
+    else
+      Game.all
+    end
+  end
+
+  def self.complexity(complexity)
+    unless complexity.blank?
+      complexity = complexity.to_i
+      tolerence = 1
+      min_complexity = complexity - 1
+      max_complexity = complexity + 1
+      Game.where(complexity: min_complexity..max_complexity)
+    else
+      Game.all
+    end
+  end
 end
