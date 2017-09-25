c201aa07c049bc5056c5738d9191fa3e60961067
c1505 <coreym247@gmail.com>
2017-09-25 01:13:29 +0000

    added clear button for form

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,9 @@
 $( document ).on('turbolinks:load', function() {
-$("#checkAll").change(function () {
-    $("input:checkbox").prop('checked', $(this).prop("checked"));
-});
-
+  $("#checkAll").change(function () {
+      $("input:checkbox").prop('checked', $(this).prop("checked"));
+  });
+  $("#clear").click(function () {
+    var id = $("#clear").data().id
+    window.location.href = "/collections/" + id
+  });
 });
