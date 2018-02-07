2c31d75d300df3ac08e16915503c58810cec6699
c1505 <coreym247@gmail.com>
2018-02-07 21:36:18 +0000

    Revert "Merge branch 'master' of https://github.com/c1505/game-chooser"
    This reverts commit eb5a7f0c7697414574b5d5a727522051b9a8f90b, reversing
    changes made to 0f9846fc72b592538828fd9650480da3cd5247b3.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,4 +2,8 @@ $( document ).on('turbolinks:load', function() {
   $("#checkAll").change(function () {
       $("input:checkbox").prop('checked', $(this).prop("checked"));
   });
+  $("#clear").click(function () {
+    var id = $("#clear").data().id
+    window.location.href = "/collections/" + id
+  });
 });
